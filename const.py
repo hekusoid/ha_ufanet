6a216a46ab9aff923a9329b015f7e4a695b22938
Kirill Khamitov <hekusoid@gmail.com>
2025-09-21 22:42:26 +0500

    1

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 """Constants for Ufanet Door Phone integration."""
 
-DOMAIN = "hekus_doorphone1"
+DOMAIN = "hekus_doorphone"
 CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_DEVICE_ID = "device_id"
